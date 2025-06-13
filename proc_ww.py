45962e98acfd13d24bd5e71dddef9f7869608bc8
owencking <92190360+owencking@users.noreply.github.com>
2025-06-13 18:05:47 -0400

    refine sentence-level segmenter

--- proc_ww.py
+++ proc_ww.py
@@ -49,38 +49,98 @@ def seg_toks( mmif_str, ww_view_id):
     """
 
     usemmif = Mmif(mmif_str)
-
     ww_view = usemmif.get_view_by_id(ww_view_id)
 
+    # get relevant MMIF annotations
     tfanns = ww_view.get_annotations(AnnotationTypes.TimeFrame)
     toanns = ww_view.get_annotations("http://vocab.lappsgrid.org/Token")
     alanns = ww_view.get_annotations(AnnotationTypes.Alignment)
     stanns = ww_view.get_annotations("http://vocab.lappsgrid.org/Sentence")
 
+    # build lists from annotations
     tfs = [ [ ann.get_property("id"), 
               ann.get_property("start"), 
               ann.get_property("end") ] for ann in tfanns 
                                         if ann.get_property("frameType") == "speech" ]
-
     tos = [ [ ann.get_property("id"),
               ann.get_property("word") ] for ann in toanns ]
-    
     als = [ [ ann.get_property("source"),
               ann.get_property("target") ] for ann in alanns ]
-
     sts = []
     for ann in stanns:
         for tg in ann.get_property("targets"):
             sts.append([ tg, ann.get_property("id") ])
 
+    # make lists into dataframes
     tfs_df = pd.DataFrame(tfs, columns=['tf_id','start','end'])
     tos_df = pd.DataFrame(tos, columns=['to_id','word'])
     als_df = pd.DataFrame(als, columns=['tf_id','to_id'])
     sts_df = pd.DataFrame(sts, columns=['to_id','st_id'])
 
-    tfs_tos_df = pd.merge( pd.merge( tfs_df, pd.merge(tos_df,als_df) ), sts_df )
+    # perform joins
+    tfs_tos_df = pd.merge( tfs_df, pd.merge(tos_df,als_df) )
+    if len(sts):
+        tfs_tos_df = pd.merge( tfs_tos_df, sts_df, how='left' )
+    else:
+        tfs_tos_df['st_id'] = None
 
-    return tfs_tos_df
+    # discard uncessary columns
+    del tfs_tos_df['tf_id']
+    del tfs_tos_df['to_id']
+
+    # want to return a list instead of a dataframe
+    toks = tfs_tos_df.values.tolist()
+
+    return toks
+
+
+def split_sts( toks, max_chars ):
+    pass
+
+
+
+
+def seg_sts( toks ):
+    """
+    Takes the token arrary and combines tokens into their sentences.
+    """
+    
+    no_space_before = ['.', ',', '-', '/']
+
+    # empty list of sentences
+    sts = []
+
+    # begin first sentence
+    start = toks[0][0]
+    end = toks[0][1]
+    se = toks[0][2]
+    se_id = toks[0][3]
+
+    for t in toks[1:]:
+        if t[3] == se_id:
+            # Same sentence.  Extend sentence string
+            end = t[1]
+            if t[2][0] in no_space_before:
+                se += t[2]
+            else:
+                se += " " + t[2]
+        else:
+            # New sentence id.  
+            # Append current sentence to the list
+            sts.append([start, end, se])
+            # Start new sentence
+            start = t[0]
+            end = t[1]
+            se = t[2]
+            se_id = t[3]
+
+    # append final sentence to the list
+    sts.append([start, end, se])
+
+    return sts
+        
+
+
 
 
 
