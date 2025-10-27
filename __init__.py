c44ec15e9fe7d26c25886d85c55a1f7255629c08
owencking <92190360+owencking@users.noreply.github.com>
2025-10-26 23:20:26 -0400

    separated out CLI-specific code into its own file

--- __init__.py
+++ __init__.py
@@ -1,1 +1,2 @@
 VERSION = "0.72"
+from .convert import mmif_to_all
--- convert.py
+++ convert.py
@@ -19,8 +19,7 @@ import json
 from datetime import datetime
 import time
 import copy
-import argparse
-import warnings
+
 
 from mmif import Mmif
 from mmif import View
@@ -538,95 +537,4 @@ def make_tpme_text( item_id:str,
     return text
 
 
-############################################################################
-# Main function
 
-def main():
-
-    app_desc = f"MMIF transcript_converter (version {VERSION}). "
-    app_desc += """
-Performs transcript conversion from MMIF to AAPB transcript JSON.
-
-This module is primarily intended to be invoked by other modules by importing it and calling the `mmif_to_all` function.
-Only limited functionality is exposed by this CLI.
-    """
-
-    parser = argparse.ArgumentParser(
-        prog='convert.py',
-        description=app_desc,
-        formatter_class=argparse.RawTextHelpFormatter
-    )
-
-    parser.add_argument("mmifpath", metavar="MMIF",
-        help="Path to the source MMIF file")
-    parser.add_argument("-v", "--vtt", action="store_true",
-        help="Output transcript in WebVTT format instead of AAPB JSON")
-    parser.add_argument("-m", "--tpme", action="store_true",
-        help="Output TPME metadata sidecars for the transcripts produced.")
-    parser.add_argument("-s", "--max-seg-chars", type=int, default=DEFAULT_MAX_SEGMENT_CHARS,
-        help="Maximum number of characters in a time-aligned segment")
-    parser.add_argument("-l", "--max-line-chars", type=int, default=DEFAULT_MAX_LINE_CHARS,
-        help="Maximum number of charcters in a line before a line break in WebVTT")
-    parser.add_argument("-i", "--item-id", default="",
-        help="The identifier of the media, to be recorded in TPME.")
-    parser.add_argument("-p", "--provider", default=DEFAULT_TPME_PROVIDER,
-        help="The transcript provider, to be recorded in TPME.")
-    parser.add_argument("-n", "--processing-note", default="",
-        help="Processing note to be recorded in TPME.")
-    
-    args = parser.parse_args()
-
-    try:
-        mmif_filename = args.mmifpath.split("/")[-1]
-        with open(args.mmifpath, "r") as file:
-            mmif_str = file.read()
-    except Exception as e:
-        print("Failed to open source MMIF file.  Encountered exception:")
-        print(e)
-        print("Use `-h` flag to see usage instructions.")
-
-    # suppress MMIF warnings
-    warnings.filterwarnings("ignore")
-
-    # perform conversion
-    tdict = mmif_to_all( mmif_str = mmif_str,
-                         item_id = args.item_id,
-                         mmif_filename = mmif_filename,
-                         tpme_provider = args.provider,
-                         max_segment_chars = args.max_seg_chars,
-                         max_line_chars = args.max_line_chars,
-                         embed_tpme_aajson = True,
-                         processing_note = args.processing_note )
-
-    # get potentially more informative media ID
-    item_id = tdict["item_id"]
-
-    # write out file(s)
-    if args.vtt:
-        # write out WebVTT
-        fname = item_id + "-transcript.vtt"
-        with open(fname, "w") as file:
-            file.write(tdict["transcript_webvtt"])
-        if args.tpme:
-            dt = datetime.now()
-            tpme_ts = f"{dt.year:04d}{dt.month:02d}{dt.day:02d}-{dt.hour:02d}{dt.minute:02d}{dt.second:02d}-{dt.microsecond:06d}"
-            tpme_fname = f'{item_id}-tpme-{tpme_ts}.json'
-            with open(tpme_fname, "w") as file:
-                file.write(tdict["tpme_webvtt"])
-    else:
-        # write out AAPB JSON
-        fname = item_id + "-transcript.json"
-        with open(fname, "w") as file:
-            file.write(tdict["transcript_aajson"])
-        if args.tpme:
-            dt = datetime.now()
-            tpme_ts = f"{dt.year:04d}{dt.month:02d}{dt.day:02d}-{dt.hour:02d}{dt.minute:02d}{dt.second:02d}-{dt.microsecond:06d}"
-            tpme_fname = f'{item_id}-tpme-{tpme_ts}.json'
-            with open(tpme_fname, "w") as file:
-                file.write(tdict["tpme_aajson"])
-
-#
-# Call to main function for stand-alone execution
-#
-if __name__ == "__main__":
-    main()
